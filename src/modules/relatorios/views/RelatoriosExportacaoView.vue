5545c07212355b325b21bfae74e4c6c279d96e9c
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-28 16:32:32 -0300

    feat: isola experiencia de exportacao da tela de relatorios

---
+++ src/modules/relatorios/views/RelatoriosExportacaoView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import RelatorioExportacaoBar from '@/modules/relatorios/components/RelatorioExportacaoBar.vue'
+import { ultimaConsultaRelatorio } from '@/modules/relatorios/services/relatorioService'
+import RelatoriosGestaoView from '@/modules/relatorios/views/RelatoriosGestaoView.vue'
+
+function limparExportacaoAoTrocarRelatorio(event: MouseEvent) {
+  const alvo = event.target
+  if (!(alvo instanceof Element)) return
+  if (alvo.closest('.relatorio-opcao')) {
+    ultimaConsultaRelatorio.value = null
+  }
+}
+</script>
+
+<template>
+  <div class="relatorios-exportacao-view" @click.capture="limparExportacaoAoTrocarRelatorio">
+    <RelatoriosGestaoView />
+    <RelatorioExportacaoBar />
+  </div>
+</template>
+
+<style scoped>
+.relatorios-exportacao-view :deep(.exportacao-footer) {
+  display: none !important;
+}
+</style>
