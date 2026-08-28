9833f3f073a9629accd6af03cea35432719daaa6
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-28 11:09:30 -0300

    feat: adiciona rota de movimentacoes

--- src/router/index.ts
+++ src/router/index.ts
@@ -77,6 +77,11 @@ export const router = createRouter({
           component: () => import('@/modules/estoque/views/EstoqueDetalheView.vue'),
         },
         {
+          path: 'movimentacoes',
+          name: 'gestao-movimentacoes',
+          component: () => import('@/modules/movimentacoes/views/MovimentacoesGestaoView.vue'),
+        },
+        {
           path: 'solicitacoes/novo',
           name: 'gestao-novo-pedido',
           component: () => import('@/modules/pedidos/views/solicitante/NovoPedidoView.vue'),
