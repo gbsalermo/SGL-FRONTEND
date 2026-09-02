190f58803512cf65dc25b818cb3eb7affd898c44
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-02 14:18:45 -0300

    feat(produtos): exibir atalho contextual de rotulo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import ProdutoRotuloAtalho from '@/modules/produtos/components/ProdutoRotuloAtalho.vue'
+</script>
+
 <template>
   <v-app>
     <router-view />
+    <ProdutoRotuloAtalho />
   </v-app>
 </template>
